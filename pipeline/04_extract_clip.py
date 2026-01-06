989189a02012a0b048be2eeec2df494c113a21d7
Aman Mulani <86338024+amaan7744@users.noreply.github.com>
2026-01-06 16:08:00 +0530

    Create 04_extract_clip.py

---
+++ pipeline/04_extract_clip.py
@@ -0,0 +1,13 @@
+from moviepy.editor import VideoFileClip
+from pipeline.03b_extract_best_segment import extract_best_segment
+
+def extract_clip(path, video_id):
+    clip = VideoFileClip(path)
+
+    start, end = extract_best_segment(video_id)
+    short = clip.subclip(start, end)
+
+    output = "data/clip.mp4"
+    short.write_videofile(output, codec="libx264", audio_codec="aac")
+
+    return output
